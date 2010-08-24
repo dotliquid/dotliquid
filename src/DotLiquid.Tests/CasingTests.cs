517809ceea8c1df0819f7bd5159b7ba290d65c52
petrhaus <alessandro@alessandropetrelli.com>
2010-08-24 21:23:34 +0800

    added casing tests

---
+++ src/DotLiquid.Tests/CasingTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using DotLiquid.Exceptions;
+using DotLiquid.NamingConventions;
+
+namespace DotLiquid.Tests
+{
+    [TestFixture]
+    public class CasingTests
+    {
+        [Test]
+        public void TestSimpleVariablesCasing()
+        {
+            Template.NamingConvention = new RubyNamingConvention();
+            Template template = Template.Parse("{{ Greeting }} {{ Name }}");
+            Assert.AreEqual("Hello Tobi", template.Render(Hash.FromAnonymousObject(new { greeting = "Hello", name = "Tobi" })));
+
+            Template.NamingConvention = new CSharpNamingConvention();
+            Assert.AreEqual("Hello Tobi", template.Render(Hash.FromAnonymousObject(new { Greeting = "Hello", Name = "Tobi" })));
+            Assert.AreEqual(" ", template.Render(Hash.FromAnonymousObject(new { greeting = "Hello", name = "Tobi" })));
+        }
+
+        [Test]
+        public void TestTagsCasingIsNotAllowed()
+        {
+            Template.NamingConvention = new RubyNamingConvention();
+            Assert.Throws<SyntaxException>(() => Template.Parse("{% IF user = 'tobi' %}Hello Tobi{% EndIf %}"));
+        }
+
+        [Test]
+        public void TestFiltersCasingIsNotAllowed()
+        {
+            Template.NamingConvention = new RubyNamingConvention();
+            Template template = Template.Parse("{{ 'hi tobi' | upcase }}");
+            Assert.AreEqual("HI TOBI", template.Render());
+
+            Template.NamingConvention = new CSharpNamingConvention();
+            template = Template.Parse("{{ 'hi tobi' | Upcase }}");
+            Assert.AreEqual("HI TOBI", template.Render());
+        }
+
+        [Test]
+        public void TestAssignsCasing()
+        {
+            Template.NamingConvention = new RubyNamingConvention();
+
+            Helper.AssertTemplateResult(".foo.", "{% assign FoO = values %}.{{ fOo[0] }}.",
+                Hash.FromAnonymousObject(new { values = new[] { "foo", "bar", "baz" } }));
+            Helper.AssertTemplateResult(".bar.", "{% assign fOo = values %}.{{ fOO[1] }}.",
+                Hash.FromAnonymousObject(new { values = new[] { "foo", "bar", "baz" } }));
+
+            Template.NamingConvention = new CSharpNamingConvention();
+
+            Helper.AssertTemplateResult(".foo.", "{% assign Foo = values %}.{{ Foo[0] }}.",
+                Hash.FromAnonymousObject(new { values = new[] { "foo", "bar", "baz" } }));
+            Helper.AssertTemplateResult(".bar.", "{% assign fOo = values %}.{{ fOo[1] }}.",
+                Hash.FromAnonymousObject(new { values = new[] { "foo", "bar", "baz" } }));
+        }
+    }
+}

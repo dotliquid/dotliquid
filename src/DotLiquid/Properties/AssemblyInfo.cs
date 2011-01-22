db165b54f0f745add5f15b577a26f08b2481655e
Tim Jones <github@roastedamoeba.com>
2011-01-22 18:02:27 +0800

    Modified build script to also generate NuGet package

--- src/DotLiquid/Properties/AssemblyInfo.cs
+++ src/DotLiquid/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FormosaTek Ltd")]
 [assembly: AssemblyProduct("DotLiquid")]
-[assembly: AssemblyCopyright("Copyright © FormosaTek Ltd and contributors 2010")]
+[assembly: AssemblyCopyright("Copyright © FormosaTek Ltd and contributors 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("DotLiquid.Tests")]

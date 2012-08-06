acdcc3db8c3bb514ccda397cfe5146f707e62423
Tim Jones <tim@timjones.tw>
2012-08-06 19:58:28 +0800

    Better fix for when WeakTable keys return negative hash codes

--- src/DotLiquid/Util/WeakTable.cs
+++ src/DotLiquid/Util/WeakTable.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace DotLiquid.Util
 {
@@ -13,7 +10,7 @@ namespace DotLiquid.Util
 			public WeakReference Value;
 		}
 
-		private Bucket[] _buckets;
+		private readonly Bucket[] _buckets;
 
 		public WeakTable(int size)
 		{
@@ -31,36 +28,29 @@ namespace DotLiquid.Util
 			}
 			set
 			{
-                int i = (int)((uint)key.GetHashCode() % (uint)_buckets.Length);
-                _buckets[i].Key = key;
+				int i = Math.Abs(key.GetHashCode()) % _buckets.Length;
+				_buckets[i].Key = key;
 				_buckets[i].Value = new WeakReference(value);
 			}
 		}
 
 		public bool TryGetValue(TKey key, out TValue value)
 		{
-            WeakReference wr;
-            try
-            {
-                int i = (int)((uint)key.GetHashCode() % (uint)_buckets.Length);
-                if ((wr = _buckets[i].Value) == null || !_buckets[i].Key.Equals(key))
-                {
-                    value = null;
-                    return false;
-                }
-                value = (TValue)wr.Target;
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                throw new IndexOutOfRangeException("Index Out Of Range, key was " + key.ToString() + ", hashcode was " + key.GetHashCode().ToString(), ex);
-            }
+			int i = Math.Abs(key.GetHashCode()) % _buckets.Length;
+			WeakReference wr;
+			if ((wr = _buckets[i].Value) == null || !_buckets[i].Key.Equals(key))
+			{
+				value = null;
+				return false;
+			}
+			value = (TValue)wr.Target;
 			return wr.IsAlive;
 		}
 
 		public void Remove(TKey key)
 		{
-            int i = (int)((uint)key.GetHashCode() % (uint)_buckets.Length);
-            if (_buckets[i].Key.Equals(key))
+			int i = Math.Abs(key.GetHashCode()) % _buckets.Length;
+			if (_buckets[i].Key.Equals(key))
 				_buckets[i].Value = null;
 		}
 	}

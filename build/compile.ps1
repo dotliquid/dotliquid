fd43c253cc1a56ad9df349908c0637c86423c7f6
Tim Jones <tim@timjones.tw>
2013-09-10 18:42:33 +0800

    Remove obsolete build files / NuGet packages.
    (Build is now done in TeamCity)

--- build/compile.ps1
+++
@@ -1,5 +0,0 @@
-task Default -depends Compile
-
-task Compile {
-    exec { msbuild /t:Clean /t:Build /p:Configuration=$config /v:q /nologo $source_dir\$solution_file }
-}
--- build/test.ps1
+++
@@ -1,9 +0,0 @@
-properties {
-    $nunit_dir = "$base_dir\src\packages\NUnit.Runners.2.6.0.12051\tools"
-}
-
-task Default -depends Test
-
-task Test {
-    exec { & $nunit_dir\nunit-console-x86.exe $source_dir/DotLiquid.Tests/$bin_folder/DotLiquid.Tests.dll /nologo /nodots /xml=$source_dir/DotLiquid.Tests/$bin_folder/DotLiquid.Tests.TestResults.xml }
-}

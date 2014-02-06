df37a1043c0bf2ef0585f9504375d45e7c3dd648
Rodrigo Dumont <rlsdumont@gmail.com>
2014-02-06 13:59:27 -0200

    Fix strftime test that was broken for %W

--- src/DotLiquid.Tests/Util/StrFTimeTests.cs
+++ src/DotLiquid.Tests/Util/StrFTimeTests.cs
@@ -10,31 +10,31 @@ namespace DotLiquid.Tests.Util
 	public class StrFTimeTests
 	{
 		[SetCulture("en-GB")]
-		[TestCase("%a", Result = "Mon")]
-		[TestCase("%A", Result = "Monday")]
+		[TestCase("%a", Result = "Sun")]
+		[TestCase("%A", Result = "Sunday")]
 		[TestCase("%b", Result = "Jan")]
 		[TestCase("%B", Result = "January")]
-		[TestCase("%c", Result = "Mon Jan 09 14:32:14 2012")]
-		[TestCase("%d", Result = "09")]
-		[TestCase("%e", Result = " 9")]
+		[TestCase("%c", Result = "Sun Jan 08 14:32:14 2012")]
+		[TestCase("%d", Result = "08")]
+		[TestCase("%e", Result = " 8")]
 		[TestCase("%H", Result = "14")]
 		[TestCase("%I", Result = "02")]
-		[TestCase("%j", Result = "009")]
+		[TestCase("%j", Result = "008")]
 		[TestCase("%m", Result = "01")]
 		[TestCase("%M", Result = "32")]
 		[TestCase("%p", Result = "PM")]
 		[TestCase("%S", Result = "14")]
 		[TestCase("%U", Result = "02")]
-		[TestCase("%W", Result = "03")]
-		[TestCase("%w", Result = "1")]
-		[TestCase("%x", Result = "09/01/2012")]
+		[TestCase("%W", Result = "01")]
+		[TestCase("%w", Result = "0")]
+		[TestCase("%x", Result = "08/01/2012")]
 		[TestCase("%X", Result = "14:32:14")]
 		[TestCase("%y", Result = "12")]
 		[TestCase("%Y", Result = "2012")]
 		[TestCase("%", Result = "%")]
 		public string TestFormat(string format)
 		{
-			return new DateTime(2012, 1, 9, 14, 32, 14).ToStrFTime(format);
+			return new DateTime(2012, 1, 8, 14, 32, 14).ToStrFTime(format);
 		}
 
 		[Test]

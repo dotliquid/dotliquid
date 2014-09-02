2325a5c0e6bd57e2a8945ea713d0fc82a0f6b4d6
Rodrigo Dumont <rlsdumont@gmail.com>
2014-09-02 14:24:57 -0300

    Implement interrupt exception
    The thing about an interruption is that it will not be caught even if
    RethrowErrors is false.

---
+++ src/DotLiquid/Exceptions/InterruptException.cs
@@ -0,0 +1,9 @@
+﻿namespace DotLiquid.Exceptions
+{
+    public class InterruptException : LiquidException
+    {
+        public InterruptException(string message) : base(message)
+        {
+        }
+    }
+}

0c7ff96a6e61410fc6f06a69c7ec3d9933737621
Bogdan Mart <mart.bogdan@gmail.com>
2016-06-17 01:24:54 +0300

    Added failing test

--- src/DotLiquid.Tests/FunctionFilterTests.cs
+++ src/DotLiquid.Tests/FunctionFilterTests.cs
@@ -23,6 +23,21 @@ namespace DotLiquid.Tests
         }
 
         [Test]
+        public void AddingAnonimousFunctionWithClosure()
+        {
+            _context["var"] = 2;
+            int x = 2;
+
+            // (x=(i + x)) is to forbid JITC to inline x and force it to create non-static closure
+
+            _context.AddFilter<int, string>("AddTwo", i => (x=(i + x)).ToString(CultureInfo.InvariantCulture));
+            Assert.That(new Variable("var | add_two").Render(_context), Is.EqualTo("4"));
+
+            //this is done, to forbid JITC to inline x 
+            Assert.That(x, Is.EqualTo(4));
+        }
+
+        [Test]
         public void AddingMethodInfo()
         {
             _context["var"] = 2;

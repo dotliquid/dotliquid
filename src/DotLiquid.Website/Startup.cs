f6374b3d472c53a9ef0cc6630cd96141129a50cf
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-06-22 07:10:37 +0200

    Website refresh

--- src/DotLiquid.Website/Startup.cs
+++ src/DotLiquid.Website/Startup.cs
@@ -16,6 +16,7 @@ namespace DotLiquid.Website
         {
             // Add framework services.
             services.AddMvc();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

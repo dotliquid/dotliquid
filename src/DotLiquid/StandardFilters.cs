dd27805b0fcbaf2f2bcadc1bb9fbb2bc54601cc4
nytmo <zelda@quick.cz>
2017-10-19 11:37:00 +0200

    Update StandardFilters.cs

--- src/DotLiquid/StandardFilters.cs
+++ src/DotLiquid/StandardFilters.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
+using System.Reflection;
 using DotLiquid.Util;
 
 namespace DotLiquid

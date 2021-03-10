89032368cd1c360fa98d5c5136bbb092f8959b29
Menachem Hornbacher <me@mendel.tech>
2021-03-10 15:09:23 -0500

    #423 fix DateTimeOffset Ruby rendering

--- src/DotLiquid.Tests/Util/StrFTimeTests.cs
+++ src/DotLiquid.Tests/Util/StrFTimeTests.cs
@@ -48,6 +48,44 @@ namespace DotLiquid.Tests.Util
             }
         }
 
+        [TestCase("%a", ExpectedResult = "Sun")]
+        [TestCase("%A", ExpectedResult = "Sunday")]
+        [TestCase("%b", ExpectedResult = "Jan")]
+        [TestCase("%B", ExpectedResult = "January")]
+        [TestCase("%c", ExpectedResult = "Sun Jan 08 14:32:14 2012")]
+        [TestCase("%C", ExpectedResult = "20")]
+        [TestCase("%d", ExpectedResult = "08")]
+        [TestCase("%e", ExpectedResult = " 8")]
+        [TestCase("%h", ExpectedResult = "Jan")]
+        [TestCase("%H", ExpectedResult = "14")]
+        [TestCase("%I", ExpectedResult = "02")]
+        [TestCase("%j", ExpectedResult = "008")]
+        [TestCase("%k", ExpectedResult = "14")]
+        [TestCase("%l", ExpectedResult = "2")]
+        [TestCase("%m", ExpectedResult = "01")]
+        [TestCase("%M", ExpectedResult = "32")]
+        [TestCase("%P", ExpectedResult = "pm")]
+        [TestCase("%p", ExpectedResult = "PM")]
+        [TestCase("%s", ExpectedResult = "1326033134")]
+        [TestCase("%S", ExpectedResult = "14")]
+        [TestCase("%u", ExpectedResult = "7")]
+        [TestCase("%U", ExpectedResult = "02")]
+        [TestCase("%W", ExpectedResult = "01")]
+        [TestCase("%w", ExpectedResult = "0")]
+        [TestCase("%x", ExpectedResult = "08/01/2012")]
+        [TestCase("%X", ExpectedResult = "14:32:14")]
+        [TestCase("%y", ExpectedResult = "12")]
+        [TestCase("%Y", ExpectedResult = "2012")]
+        [TestCase("%", ExpectedResult = "%")]
+        public string TestOffsetFormat(string format)
+        {
+            using (CultureHelper.SetCulture("en-GB"))
+            {
+                Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
+                return new DateTimeOffset(2012, 1, 8, 14, 32, 14, TimeSpan.FromHours(0)).ToStrFTime(format);
+            }
+        }
+
         [Test]
         public void TestTimeZone()
         {

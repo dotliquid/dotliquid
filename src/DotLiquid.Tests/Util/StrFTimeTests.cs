31d54966ea84c1b38af1b9b88f86370aa30cffda
Robin Parker <robin.parker@msamlin.com>
2021-05-26 14:25:00 +0000

    Strftime enhancements, Resolves #251

--- src/DotLiquid.Tests/Util/StrFTimeTests.cs
+++ src/DotLiquid.Tests/Util/StrFTimeTests.cs
@@ -10,39 +10,77 @@ namespace DotLiquid.Tests.Util
     [TestFixture]
     public class StrFTimeTests
     {
-        [TestCase("%a", ExpectedResult = "Sun")]
-        [TestCase("%A", ExpectedResult = "Sunday")]
-        [TestCase("%b", ExpectedResult = "Jan")]
-        [TestCase("%B", ExpectedResult = "January")]
-        [TestCase("%c", ExpectedResult = "Sun Jan 08 14:32:14 2012")]
-        [TestCase("%C", ExpectedResult = "20")]
-        [TestCase("%d", ExpectedResult = "08")]
-        [TestCase("%e", ExpectedResult = " 8")]
-        [TestCase("%h", ExpectedResult = "Jan")]
-        [TestCase("%H", ExpectedResult = "14")]
-        [TestCase("%I", ExpectedResult = "02")]
-        [TestCase("%j", ExpectedResult = "008")]
-        [TestCase("%k", ExpectedResult = "14")]
-        [TestCase("%l", ExpectedResult = "2")]
-        [TestCase("%m", ExpectedResult = "01")]
-        [TestCase("%M", ExpectedResult = "32")]
-        [TestCase("%P", ExpectedResult = "pm")]
-        [TestCase("%p", ExpectedResult = "PM")]
-        [TestCase("%S", ExpectedResult = "14")]
-        [TestCase("%u", ExpectedResult = "7")]
-        [TestCase("%U", ExpectedResult = "02")]
-        [TestCase("%W", ExpectedResult = "01")]
-        [TestCase("%w", ExpectedResult = "0")]
-        [TestCase("%x", ExpectedResult = "08/01/2012")]
-        [TestCase("%X", ExpectedResult = "14:32:14")]
-        [TestCase("%y", ExpectedResult = "12")]
-        [TestCase("%Y", ExpectedResult = "2012")]
+        // Descriptions below are taken from the Ruby Time.strftime documentation.
+        [TestCase("%a", ExpectedResult = "Sun")] // Abbreviated weekday name (Mon)
+        [TestCase("%A", ExpectedResult = "Sunday")] // Full weekday name (Monday)
+        [TestCase("%b", ExpectedResult = "Jan")] // Abbreviated month name (Jan)
+        [TestCase("%B", ExpectedResult = "January")] // Full month name (January)
+        [TestCase("%^B", ExpectedResult = "JANUARY")] // Upcased month name
+        [TestCase("%^_10B", ExpectedResult = "   JANUARY")] // Upcase, then Space-pad
+        [TestCase("%_^10B", ExpectedResult = "   JANUARY")] // Space-pad, then Upcase
+        [TestCase("%c", ExpectedResult = "Sun Jan 08 14:32:14 2012")] // Date and time representation (Thu Aug 23 14:55:02 2001)
+        [TestCase("%C", ExpectedResult = "20")] //Century (20 in 2009)
+        [TestCase("%d", ExpectedResult = "08")] // Day of Month, zero-padded (01..31)
+        [TestCase("%-d", ExpectedResult = "8")] // Remove zero-padding
+        [TestCase("%D", ExpectedResult = "01/08/12")] // Short date (%m/%d/%y)
+        [TestCase("%e", ExpectedResult = " 8")] // Day of month, space-padded ( 1..31)
+        // %E - not specified
+        // %f - not specified
+        [TestCase("%F", ExpectedResult = "2012-01-08")] // ISO 8601 date (2001-08-23)
+        [TestCase("%g", ExpectedResult = "12")] // Week-based year, last two digits (00..99)
+        [TestCase("%G", ExpectedResult = "2012")]  // Week based year (2001)
+        [TestCase("%h", ExpectedResult = "Jan")] // Abbreviated month name (Jan)
+        [TestCase("%H", ExpectedResult = "14")] // Hour in 24h format, zero-padded (00..23)
+        // %i - not specified
+        [TestCase("%I", ExpectedResult = "02")] // Hour in 12h format, zero-padded (01..12)
+        [TestCase("%j", ExpectedResult = "008")] // Day of year (001..366)
+        // %J - not specified
+        [TestCase("%k", ExpectedResult = "14")] //Hour in 24-hour format, blank-padded ( 0..23)
+        // %K - not specified
+        [TestCase("%l", ExpectedResult = " 2")] //Hour in 12-hour format, blank-padded ( 0..12)
+        [TestCase("%L", ExpectedResult = "123")] // Millisecond of the second (000..999). The digits under millisecond are truncated to not produce 1000.
+        [TestCase("%m", ExpectedResult = "01")] // Month of the year, zero-padded (01..12)
+        [TestCase("%-m", ExpectedResult = "1")] // Remove zero-padding
+        [TestCase("%_m", ExpectedResult = " 1")] // Replace zero-pad with space-padding
+        [TestCase("%_3m", ExpectedResult = "  1")] // Replace single zero-pad with multi-space-pad
+        [TestCase("%010m", ExpectedResult = "0000000001")] // Replace single zero-pad with multi-zero-pad
+        [TestCase("%M", ExpectedResult = "32")] // Minute of the hour (00..59)
+        [TestCase("%n", ExpectedResult = "\n")] // new-line character (\n)
+        [TestCase("%N", ExpectedResult = "123000")] // Fractional seconds digits, default is 9 digits (nanosecond) --> C# DateTime does not support nanosecond precision
+        [TestCase("%3N", ExpectedResult = "123")] // ... millisecond (3 digits)
+        [TestCase("%6N", ExpectedResult = "123000")] // ... microsecond (6 digits)
+        // %9N - nanosecond (9 digits) --> C# DateTime does not support nanosecond precision
+        // %o - not specified
+        // %O - not specified
+        [TestCase("%p", ExpectedResult = "PM")] // Meridian indicator, uppercase (AM/PM)
+        [TestCase("%P", ExpectedResult = "pm")] // Meridian indicator, lowercase (am/pm)
+        // %q - not specified
+        // %Q - not specified
+        [TestCase("%r", ExpectedResult = "02:32:14 PM")] // 12-hour clock time (02:55:02 PM)
+        [TestCase("%R", ExpectedResult = "14:32")] // 24-hour HH:MM time, equivalent to %H:%M (14:55)
+        // %s - see TestEpoch
+        [TestCase("%S", ExpectedResult = "14")] //Second (00..60)
+        [TestCase("%t", ExpectedResult = "\t")] // Tab character (\t)
+        [TestCase("%T", ExpectedResult = "14:32:14")] // ISO 8601 time format (14:55:02)
+        [TestCase("%u", ExpectedResult = "7")] // Day of the week, Monday is 1 (1..7)
+        [TestCase("%U", ExpectedResult = "02")] // Week of the year, Sunday as the first day of week one (01..53)
+        [TestCase("%v", ExpectedResult = " 8-JAN-2012")] // VMS date (%e-%^b-%4Y)
+        [TestCase("%V", ExpectedResult = "01")] // Week number of the week-based year (01..53)
+        [TestCase("%w", ExpectedResult = "0")] // Day of the week, Sunday is 0 (0..6)
+        [TestCase("%W", ExpectedResult = "01")] // Week number with the first Monday as the first day of week one (00..53)
+        [TestCase("%x", ExpectedResult = "01/08/12")] // Locale Date representation (08/23/01)
+        [TestCase("%X", ExpectedResult = "14:32:14")] // Locale Time representation (14:55:02)
+        [TestCase("%y", ExpectedResult = "12")] // Year, last two digits (00-99)
+        [TestCase("%Y", ExpectedResult = "2012")] // Year with century (2001)
+        [TestCase("%_2Y", ExpectedResult = "2012")] // Ensure space-pad does not truncate.
+        // %z, %:z, %::z --> See TestTimeZoneUTC/TestTimeZoneLocal
         [TestCase("%", ExpectedResult = "%")]
         public string TestFormat(string format)
         {
-            using (CultureHelper.SetCulture("en-GB")) {
+            using (CultureHelper.SetCulture("en-GB"))
+            {
                 Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
-                var date = new DateTime(2012, 1, 8, 14, 32, 14);
+                var date = new DateTime(2012, 1, 8, 14, 32, 14, 123);
                 var localResult = date.ToStrFTime(format);
                 var utcResult = new DateTimeOffset(date, TimeSpan.FromHours(0)).ToStrFTime(format);
                 Assert.AreEqual(localResult, utcResult);
@@ -53,12 +91,13 @@ namespace DotLiquid.Tests.Util
         }
 
         [Test]
+        // %s - Gives number of seconds since 1970-01-01 00:00:00 UTC
         public void TestEpoch()
         {
             using (CultureHelper.SetCulture("en-GB"))
             {
                 Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
-                var date = new DateTime(2012, 1, 8, 14, 32, 14);
+                var date = new DateTime(2012, 1, 8, 14, 32, 14, 123);
                 var localResult = date.ToStrFTime("%s");
                 Assert.AreEqual("1326033134", localResult);
                 var utcResult = new DateTimeOffset(date, TimeSpan.FromHours(0)).ToStrFTime("%s");
@@ -74,6 +113,41 @@ namespace DotLiquid.Tests.Util
             var now = DateTimeOffset.Now;
             string timeZoneOffset = now.ToString("zzz");
             Assert.That(now.DateTime.ToStrFTime("%Z"), Is.EqualTo(timeZoneOffset));
+        }
+
+        [TestCase("%z", ExpectedResult = "Z")] // ISO 8601 offset from UTC in timezone (1 minute=1, 1 hour=100) If timezone cannot be determined, no characters (+100)
+        [TestCase("%:z", ExpectedResult = "Z")] // hour and minute offset from UTC with a colon (e.g. +09:00)
+        public string TestTimeZoneUTC(string format)
+        {
+            using (CultureHelper.SetCulture("en-GB"))
+            {
+                Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
+                return DateTime.UtcNow.ToStrFTime(format);
+            }
+        }
+
+        [TestCase("%z", ExpectedResult = "+0100")] // ISO 8601 offset from UTC in timezone (1 minute=1, 1 hour=100) If timezone cannot be determined, no characters (+100)
+        [TestCase("%:z", ExpectedResult = "+01:00")] // hour and minute offset from UTC with a colon (e.g. +09:00)
+        public string TestTimeZoneLocal(string format)
+        {
+            using (CultureHelper.SetCulture("en-GB"))
+            {
+                Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
+                // Jun-10 is during UK British Summer Time (BST / +01:00)
+                return DateTimeOffset.Parse("2012-06-10T14:32:14+01:00").ToStrFTime(format);
+            }
+        }
+
+        [TestCase("%G", ExpectedResult = "2013")] // ISO 8601 offset from UTC in timezone (1 minute=1, 1 hour=100) If timezone cannot be determined, no characters (+100)
+        [TestCase("%g", ExpectedResult = "13")] // hour and minute offset from UTC with a colon (e.g. +09:00)
+        [TestCase("%V", ExpectedResult = "01")] // hour and minute offset from UTC with a colon (e.g. +09:00)
+        public string TestIso8601WeekBasedDates(string format)
+        {
+            using (CultureHelper.SetCulture("en-GB"))
+            {
+                Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
+                return DateTime.Parse("2012-12-31").ToStrFTime(format);
+            }
         }
     }
 }
--- src/DotLiquid/Util/StrFTime.cs
+++ src/DotLiquid/Util/StrFTime.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace DotLiquid.Util
 {
@@ -18,85 +20,197 @@ namespace DotLiquid.Util
             { "c", (dateTime) => dateTime.ToString("ddd MMM dd HH:mm:ss yyyy", CultureInfo.CurrentCulture) },
             { "C", (dateTime) => ((int)Math.Floor(Convert.ToDouble(dateTime.ToString("yyyy"))/100)).ToString() },
             { "d", (dateTime) => dateTime.ToString("dd", CultureInfo.CurrentCulture) },
+            { "D", (dateTime) => dateTime.ToString("MM/dd/yy", CultureInfo.CurrentCulture) },
             { "e", (dateTime) => dateTime.ToString("%d", CultureInfo.CurrentCulture).PadLeft(2, ' ') },
+            // E - not specified
+            // f - not specified
+            { "F", (dateTime) => dateTime.ToString("yyyy-MM-dd", CultureInfo.CurrentCulture) },
+            { "g", (dateTime) => dateTime.GetIso8601WeekOfYear("g") },
+            { "G", (dateTime) => dateTime.GetIso8601WeekOfYear("G") },
             { "h", (dateTime) => dateTime.ToString("MMM", CultureInfo.CurrentCulture) },
             { "H", (dateTime) => dateTime.ToString("HH", CultureInfo.CurrentCulture) },
+            // i - not specified
             { "I", (dateTime) => dateTime.ToString("hh", CultureInfo.CurrentCulture) },
             { "j", (dateTime) => dateTime.DayOfYear.ToString().PadLeft(3, '0') },
-            { "m", (dateTime) => dateTime.ToString("MM", CultureInfo.CurrentCulture) },
+            // J - not specified
             { "k", (dateTime) => dateTime.ToString("%H", CultureInfo.CurrentCulture) },
-            { "l", (dateTime) => dateTime.ToString("%h", CultureInfo.CurrentCulture) },
+            // K - not specified
+            { "l", (dateTime) => dateTime.ToString("%h", CultureInfo.CurrentCulture).PadLeft(2, ' ') },
+            { "L", (dateTime) => dateTime.ToString("fff", CultureInfo.CurrentCulture) },
+            { "m", (dateTime) => dateTime.ToString("MM", CultureInfo.CurrentCulture) },
             { "M", (dateTime) => dateTime.Minute.ToString().PadLeft(2, '0') },
-            { "P", (dateTime) => dateTime.ToString("tt", CultureInfo.CurrentCulture).ToLower() },
+            { "n", (dateTime) => "\n" },
+            { "N", (dateTime) => dateTime.ToString("ffffff", CultureInfo.CurrentCulture) }, //The Ruby spec states default=nanoseconds, but nanosecond precision is not supported by a C# DateTime
+            { "3N", (dateTime) => dateTime.ToString("fff", CultureInfo.CurrentCulture) },
+            { "6N", (dateTime) => dateTime.ToString("ffffff", CultureInfo.CurrentCulture) },
+            // 9N - not implemented
+            // o - not specified
+            // O - not specified
             { "p", (dateTime) => dateTime.ToString("tt", CultureInfo.CurrentCulture).ToUpper() },
+            { "P", (dateTime) => dateTime.ToString("tt", CultureInfo.CurrentCulture).ToLower() },
+            // q - not specified
+            // Q - not specified
+            { "r", (dateTime) => dateTime.ToString("hh:mm:ss tt", CultureInfo.CurrentCulture).ToUpper() },
+            { "R", (dateTime) => dateTime.ToString("HH:mm", CultureInfo.CurrentCulture) },
             { "s", (dateTime) => ((int)(dateTime - new DateTime(1970, 1, 1)).TotalSeconds).ToString() },
             { "S", (dateTime) => dateTime.ToString("ss", CultureInfo.CurrentCulture) },
+            { "t", (dateTime) => "\t" },
+            { "T", (dateTime) => dateTime.ToString("HH:mm:ss", CultureInfo.CurrentCulture) },
             { "u", (dateTime) => ((int)(dateTime.DayOfWeek) == 0 ? ((int)(dateTime).DayOfWeek) + 7 : ((int)(dateTime).DayOfWeek)).ToString() },
             { "U", (dateTime) => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(dateTime, CultureInfo.CurrentCulture.DateTimeFormat.CalendarWeekRule, DayOfWeek.Sunday).ToString().PadLeft(2, '0') },
-            { "W", (dateTime) => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(dateTime, CultureInfo.CurrentCulture.DateTimeFormat.CalendarWeekRule, DayOfWeek.Monday).ToString().PadLeft(2, '0') },
+            { "v", (dateTime) => dateTime.ToString("%d-MMM-yyyy", CultureInfo.CurrentCulture).ToUpper().PadLeft(11, ' ') },
+            { "V", (dateTime) => dateTime.GetIso8601WeekOfYear("V") },
             { "w", (dateTime) => ((int) dateTime.DayOfWeek).ToString() },
-            { "x", (dateTime) => dateTime.ToString("d", CultureInfo.CurrentCulture) },
-            { "X", (dateTime) => dateTime.ToString("T", CultureInfo.CurrentCulture) },
+            { "W", (dateTime) => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(dateTime, CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday).ToString().PadLeft(2, '0') },
+            { "x", (dateTime) => dateTime.ToString("MM/dd/yy", CultureInfo.CurrentCulture) },
+            { "X", (dateTime) => dateTime.ToString("HH:mm:ss", CultureInfo.CurrentCulture) },
             { "y", (dateTime) => dateTime.ToString("yy", CultureInfo.CurrentCulture) },
             { "Y", (dateTime) => dateTime.ToString("yyyy", CultureInfo.CurrentCulture) },
+            { "z", (dateTime) => dateTime.ToString("%K", CultureInfo.CurrentCulture).Replace(":", string.Empty) },
+            { ":z", (dateTime) => dateTime.ToString("%K", CultureInfo.CurrentCulture) },
+            // ::z - not implemented
             { "Z", (dateTime) => dateTime.ToString("zzz", CultureInfo.CurrentCulture) },
-            { "%", (dateTime) => "%" }
+            { "%", (dateTime) => "%" } // A % sign
         };
 
         private static readonly Dictionary<string, DateTimeOffsetDelegate> OffsetFormats = new Dictionary<string, DateTimeOffsetDelegate>
         {
             { "s", (dateTimeOffset) => ((long)(dateTimeOffset - new DateTimeOffset(1970, 1, 1, 0,0,0, TimeSpan.Zero)).TotalSeconds).ToString() },
+            { "z", (dateTimeOffset) => dateTimeOffset.ToString("%K", CultureInfo.CurrentCulture).Replace(":", string.Empty) },
+            { ":z", (dateTimeOffset) => dateTimeOffset.ToString("%K", CultureInfo.CurrentCulture) },
             { "Z", (dateTimeOffset) => dateTimeOffset.ToString("zzz", CultureInfo.CurrentCulture) },
         };
 
+        /// <summary>
+        /// Applies formatting consistent to the rules specified by the Ruby Time.strftime function.
+        /// The following exceptions apply;
+        /// - `%N` (Not standard) - DateTime does not support nanoseconds precision, so the default is microsecond (6dp).
+        /// - `%9N` (Not implemented) - DateTime does not support nanoseconds precision.
+        /// - `%Z` (Not standard) - DotLiquid returns an offset (+00:00), Ruby states this should return a timezone description (e.g. 'GMT')
+        /// <see href="https://help.shopify.com/themes/liquid/filters/additional-filters#date"/>
+        /// <see href="https://ruby-doc.org/core-3.0.0/Time.html#method-i-strftime"/>
+        /// </summary>
+        /// <param name="dateTime"/>
+        /// <param name="pattern"/>
+        /// <returns>a string version of dateTime matching pattern.</returns>
         public static string ToStrFTime(this DateTime dateTime, string pattern)
         {
-            string output = "";
-
-            int n = 0;
-
-            while (n < pattern.Length)
-            {
-                string s = pattern.Substring(n, 1);
-
-                if (n + 1 >= pattern.Length)
-                    output += s;
-                else
-                    output += s == "%"
-                        ? Formats.ContainsKey(pattern.Substring(++n, 1)) ? Formats[pattern.Substring(n, 1)].Invoke(dateTime) : "%" + pattern.Substring(n, 1)
-                        : s;
-                n++;
-            }
-
-            return output;
+            return Regex.Replace(pattern, @"%(?<flag>[-_0^:#])*(?<width>[1-9][0-9]*)?(?<directive>[a-zA-Z%\+])",
+                x => StrFTimeMatchEvaluator(
+                    x.Groups[0].Value,
+                    x.Groups["flag"].Captures.Cast<Capture>().Select(y => y.Value).ToList(),
+                    x.Groups["width"].Captures.Cast<Capture>().Select(y => (int?)Convert.ToInt32(y.Value)).FirstOrDefault(),
+                    x.Groups["directive"].Captures.Cast<Capture>().Select(y => y.Value).FirstOrDefault(),
+                    dateTime
+                    ));
         }
 
         /// <summary>
         /// Formats a date using a ruby date format string
         /// </summary>
-        /// <param name="dateTime"></param>
+        /// <param name="dateTimeOffset"></param>
         /// <param name="pattern"></param>
         /// <returns></returns>
-        public static string ToStrFTime(this DateTimeOffset dateTime, string pattern)
+        public static string ToStrFTime(this DateTimeOffset dateTimeOffset, string pattern)
         {
-            var output = new System.Text.StringBuilder();
+            return Regex.Replace(pattern, @"%(?<flag>[-_0^:#])*(?<width>[1-9][0-9]*)?(?<directive>[a-zA-Z%\+])",
+                x => StrFTimeMatchEvaluator(
+                    x.Groups[0].Value,
+                    x.Groups["flag"].Captures.Cast<Capture>().Select(y => y.Value).ToList(),
+                    x.Groups["width"].Captures.Cast<Capture>().Select(y => (int?)Convert.ToInt32(y.Value)).FirstOrDefault(),
+                    x.Groups["directive"].Captures.Cast<Capture>().Select(y => y.Value).FirstOrDefault(),
+                    dateTimeOffset
+                    ));
+        }
 
-            for (int n = 0; n < pattern.Length; n++)
+        private static String StrFTimeMatchEvaluator(String orig, IEnumerable<String> flags, int? width, String directive, object obj)
+        {
+            var result = orig;
+            directive = PreProcessDirective(directive, flags, width);
+
+            bool isDateTimeOffset = obj is DateTimeOffset dateTimeOffset;
+            if (OffsetFormats.ContainsKey(directive) && isDateTimeOffset)
+                result = OffsetFormats[directive].Invoke(dateTimeOffset);
+            else if (Formats.ContainsKey(directive))
+                result = Formats[directive].Invoke(isDateTimeOffset ? dateTimeOffset.DateTime : (DateTime)obj);
+            else
+                return orig; // This is an unspecified directive
+
+            return flags.ToList().Aggregate(result, (current, flag) => ApplyFlag(flag, (width ?? 2), current));
+        }
+
+        // Pre-process the directive for some of the quirkier cases, such as '%:z' and '%3N'.
+        private static String PreProcessDirective(String directive, IEnumerable<String> flags, int? width)
+        {
+            String result = directive;
+            String flagString = String.Join("", flags);
+            if ("z".Equals(directive) && ":".Equals(flagString))
             {
-                string s = pattern.Substring(n, 1);
+                result = flagString + directive;
+            }
+            else if ("N".Equals(directive) && (width == 3 || width == 6))
+            {
+                result = "" + width + directive;
+            }
+            return result;
+        }
 
-                if (s == "%" && pattern.Length > ++n)
-                    if (OffsetFormats.ContainsKey(pattern.Substring(n, 1)))
-                        output.Append(OffsetFormats[pattern.Substring(n, 1)].Invoke(dateTime));
-                    else if (Formats.ContainsKey(pattern.Substring(n, 1)))
-                        output.Append(Formats[pattern.Substring(n, 1)].Invoke(dateTime.DateTime));
-                    else
-                        output.Append("%" + pattern.Substring(n, 1));
-                else
-                    output.Append(s);
+        private static String ApplyFlag(String flag, int padwidth, String str)
+        {
+            var result = str;
+            switch (flag)
+            {
+                case "-": //don't pad a numerical output
+                    result = str.TrimStart('0');
+                    break;
+                case "_": //use spaces for padding
+                    result = str.TrimStart('0').PadLeft(padwidth, ' ');
+                    break;
+                case "0": //use zeros for padding
+                    result = str.TrimStart('0').PadLeft(padwidth, '0');
+                    break;
+                case "^": //upcase the result string
+                    result = str.ToUpper();
+                    break;
+                case ":": // handled by PreProcessDirective
+                    break;
+                case "#": // not implemented
+                    break;
+                    // default: do nothing.
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// ISO 8601 week-based year and week number:
+        /// - The week 1 of YYYY starts with a Monday and includes YYYY-01-04.
+        /// - The days in the year before the first week are in the last week of the previous year.
+        /// For example 2012-12-31 is in the first week of 2013 (according to ISO-8601)
+        /// <param name="dateTime"></param>
+        /// <param name="directive"></param>
+        /// </summary>
+        private static string GetIso8601WeekOfYear(this DateTime dateTime, string directive)
+        {
+            // If its Monday, Tuesday or Wednesday, then it'll be the same week
+            // as whatever Thursday, Friday or Saturday are.
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(dateTime);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                dateTime = dateTime.AddDays(3);
             }
 
-            return output.ToString();
+            switch (directive)
+            {
+                case "G":
+                    return dateTime.ToString("yyyy");
+                case "g":
+                    return dateTime.ToString("yy");
+                case "V":
+                    // Return the week number of our adjusted day
+                    return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(dateTime, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday).ToString().PadLeft(2, '0');
+                default:
+                    throw new ArgumentException(message: "Invalid directive passed to GetIso8601WeekOfYear", paramName: "directive");
+            }
         }
     }
 }

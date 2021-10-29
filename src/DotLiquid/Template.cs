04ec7ae176d6fc2ca065000467fbd5bf0903c1af
Tom Parrish <ceilingfish@gmail.com>
2021-10-29 14:08:42 +0100

    Revert unecessary change

--- src/DotLiquid/Template.cs
+++ src/DotLiquid/Template.cs
@@ -410,7 +410,7 @@ namespace DotLiquid
         {
             // Can't dispose this new StreamWriter, because it would close the
             // passed-in stream, which isn't up to us.
-            StreamWriter streamWriter = new StreamWriterWithFormatProvider(stream, parameters.FormatProvider);
+            StreamWriter streamWriter = new StreamWriterWithFormatProvider( stream, parameters.FormatProvider );
             RenderInternal(streamWriter, parameters);
             streamWriter.Flush();
         }

7b57e361f8dd143d92d4e233857ccb4aa5571a67
microalps <github@microalps.com>
2024-12-03 23:52:23 -0500

    Corrected website error handling lost in #204

--- src/DotLiquid.Website/Program.cs
+++ src/DotLiquid.Website/Program.cs
@@ -17,10 +17,13 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

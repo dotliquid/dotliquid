25e8ed56e3da5bd8adc198570f8f8ec76fe44e52
Rodney Richardson <rodney@rjrsoftware.com>
2025-02-21 13:54:29 +0000

    Remove backing fields from simple properties.

--- src/DotLiquid/RenderParameters.cs
+++ src/DotLiquid/RenderParameters.cs
@@ -38,42 +38,21 @@ namespace DotLiquid
             get { return (ErrorsOutputMode == ErrorsOutputMode.Rethrow); }
             set { ErrorsOutputMode = (value ? ErrorsOutputMode.Rethrow : ErrorsOutputMode.Display); }
         }
-        
-        private ErrorsOutputMode _erorsOutputMode = ErrorsOutputMode.Display;
 
         /// <summary>
         /// Errors output mode
         /// </summary>
-        public ErrorsOutputMode ErrorsOutputMode
-        {
-            get
-            {
-                return _erorsOutputMode;
-            }
-
-            set
-            {
-                _erorsOutputMode = value;
-            }
-        }
+        public ErrorsOutputMode ErrorsOutputMode { get; set; } = ErrorsOutputMode.Display;
 
         /// <summary>
         /// Liquid syntax flag used for backward compatibility
         /// </summary>
         public SyntaxCompatibility SyntaxCompatibilityLevel { get; set; }
 
-        private int _maxIterations = 0;
-
         /// <summary>
         /// Maximum number of iterations for the For tag
         /// </summary>
-        public int MaxIterations
-        {
-            get { return _maxIterations; }
-            set { _maxIterations = value; }
-        }
-
-        private int _timeout = 0;
+        public int MaxIterations { get; set; } = 0;
         public IFormatProvider FormatProvider { get; }
 
         public RenderParameters(IFormatProvider formatProvider)
@@ -85,11 +64,7 @@ namespace DotLiquid
         /// <summary>
         /// Rendering timeout in ms
         /// </summary>
-        public int Timeout
-        {
-            get { return _timeout; }
-            set { _timeout = value; }
-        }
+        public int Timeout { get; set; } = 0;
 
         internal void Evaluate(Template template, out Context context, out Hash registers, out IEnumerable<Type> filters)
         {

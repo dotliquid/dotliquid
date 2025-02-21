a8338dbdfe68fcd69b8463c4cb74dc196387a41d
Rodney Richardson <rodney@rjrsoftware.com>
2025-02-21 13:58:21 +0000

    Remove unnecessary cast

--- src/DotLiquid/Tags/Include.cs
+++ src/DotLiquid/Tags/Include.cs
@@ -61,9 +61,9 @@ namespace DotLiquid.Tags
                 foreach (var keyValue in _attributes)
                     context[keyValue.Key] = context[keyValue.Value];
 
-                if (variable is IEnumerable)
+                if (variable is IEnumerable values)
                 {
-                    ((IEnumerable) variable).Cast<object>().ToList().ForEach(v =>
+                    values.Cast<object>().ToList().ForEach(v =>
                     {
                         context[shortenedTemplateName] = v;
                         partial.Render(result, RenderParameters.FromContext(context, result.FormatProvider));

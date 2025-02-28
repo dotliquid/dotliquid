c7982b06f517ff4c8281094d5ad8f07d0e782fe5
Rodney Richardson <rodney@rjrsoftware.com>
2025-02-28 15:41:31 +0000

    Check for list index out of bounds.

--- src/DotLiquid.Tests/ContextTests.cs
+++ src/DotLiquid.Tests/ContextTests.cs
@@ -233,6 +233,23 @@ namespace DotLiquid.Tests
             Assert.That(_context["nil"], Is.EqualTo(null));
         }
 
+        [Test]
+        public void TestVariablesArray()
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            _context["list"] = list;
+            Assert.That(_context["list"], Is.EqualTo(list));
+            Assert.That(_context["list[0]"], Is.EqualTo(list[0]));
+            Assert.That(_context["list[-1]"], Is.EqualTo(list[list.Count - 1]));
+            Assert.That(_context["list[12]"], Is.Null);
+
+            List<string> emptyList = new List<string>();
+            _context["empty_list"] = emptyList;
+            Assert.That(_context["empty_list"], Is.EqualTo(emptyList));
+            Assert.That(_context["empty_list[0]"], Is.Null);
+            Assert.That(_context["empty_list[-1]"], Is.Null);
+        }
+
 #if NET6_0_OR_GREATER
         [Test]
         public void TestVariables_NET60()

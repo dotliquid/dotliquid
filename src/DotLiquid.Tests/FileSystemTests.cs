378d81e441a05523d0c83b99a62b7eb0459f6b26
Tobias Lekman <tobias@lekman.com>
2025-08-14 07:47:42 +0100

    fix: Make FileSystemTests cross-platform compatible
    - Replace hardcoded Windows paths with OS-specific paths using RuntimeInformation
    - Use Path.Combine for all path operations to ensure correct separators
    - Tests now detect the OS and use appropriate paths (Windows or Unix)
    - Uncommented and fixed all previously disabled tests in FileSystemTests.cs
    
    Fixes #issue - FilePathTests failing on Ubuntu due to Windows-style paths

--- src/DotLiquid.Tests/FileSystemTests.cs
+++ src/DotLiquid.Tests/FileSystemTests.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using DotLiquid.Exceptions;
+using DotLiquid.FileSystems;
 using NUnit.Framework;
 
 namespace DotLiquid.Tests
@@ -5,78 +13,101 @@ namespace DotLiquid.Tests
     [TestFixture]
     public class FileSystemTests
     {
-        //private readonly string[] invalidPaths = new[]
-        //{
-        //    "\root\file",
-        //    "folder/",
-        //    "root/.hidden",
-        //    ".hidden",
-        //    "../test",
-        //    "../dir/mypartial",
-        //    "/dir/../../dir/mypartial",
-        //    "/etc/passwd",
-        //    "C:\\mypartial",
-        //    "",
-        //    "some\r\nmultiline"
-        //};
+        private readonly string[] invalidPaths = new[]
+        {
+            Path.DirectorySeparatorChar + "root" + Path.DirectorySeparatorChar + "file",
+            "folder/",
+            "root/.hidden",
+            ".hidden",
+            "../test",
+            "../dir/mypartial",
+            "/dir/../../dir/mypartial",
+            "/etc/passwd",
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"C:\mypartial" : "/home/mypartial",
+            "",
+            "some\r\nmultiline"
+        };
 
-        //private readonly Dictionary<string, string> validPaths = new Dictionary<string, string>()
-        //{
-        //    { @"root/file.txt", @"root\_file.txt.liquid" },
-        //    { @"root\file.txt", @"root\_file.txt.liquid" },
-        //    { @"dir/mypartial", @"dir\_mypartial.liquid" },
-        //    { @"dir\mypartial", @"dir\_mypartial.liquid" },
-        //    { @"dir\subdir\mypartial", @"dir\subdir\_mypartial.liquid" },
-        //    { @"mypartial", @"_mypartial.liquid" },
-        //    { @"a", @"_a.liquid" },
-        //    { @"a/b", @"a\_b.liquid" },
-        //    { @"-abc", @"_-abc.liquid" },
-        //    { @"a-bc",  @"_a-bc.liquid" }
-        //};
+        private readonly Dictionary<string, string> validPaths = new Dictionary<string, string>()
+        {
+            { @"root/file.txt", Path.Combine("root", "_file.txt.liquid") },
+            { @"root\file.txt", Path.Combine("root", "_file.txt.liquid") },
+            { @"dir/mypartial", Path.Combine("dir", "_mypartial.liquid") },
+            { @"dir\mypartial", Path.Combine("dir", "_mypartial.liquid") },
+            { @"dir\subdir\mypartial", Path.Combine("dir", "subdir", "_mypartial.liquid") },
+            { @"mypartial", "_mypartial.liquid" },
+            { @"a", "_a.liquid" },
+            { @"a/b", Path.Combine("a", "_b.liquid") },
+            { @"-abc", "_-abc.liquid" },
+            { @"a-bc", "_a-bc.liquid" }
+        };
 
-        //[Test]
-        //public void TestDefault()
-        //{
-        //    Assert.Throws<FileSystemException>(() => new BlankFileSystem().ReadTemplateFile(new Context(CultureInfo.InvariantCulture), "dummy"));
-        //}
+        [Test]
+        public void TestDefault()
+        {
+            Assert.Throws<FileSystemException>(() => new BlankFileSystem().ReadTemplateFile(new Context(CultureInfo.InvariantCulture), "dummy"));
+        }
 
+        [Test]
+        public void TestLocalCrossPlatform()
+        {
+            // Use a cross-platform base path
+            string basePath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) 
+                ? @"C:\Some\Path" 
+                : "/home/some/path";
+            
+            LocalFileSystem fileSystem = new LocalFileSystem(basePath);
+            
+            foreach (var validPath in validPaths)
+            {
+                string expectedPath = Path.Combine(basePath, validPath.Value);
+                Assert.AreEqual(
+                    expected: expectedPath,
+                    actual: fileSystem.FullPath(validPath.Key));
+            }
 
-        //[Test]
-        //[Category("windows")]
-        //public void TestLocal()
-        //{
-        //    LocalFileSystem fileSystem = new LocalFileSystem(@"D:\Some\Path");
-        //    foreach (var validPath in validPaths)
-        //        Assert.AreEqual(
-        //            expected: Path.Combine(@"D:\Some\Path", validPath.Value.Replace('\\', Path.DirectorySeparatorChar)),
-        //            actual: fileSystem.FullPath(validPath.Key));
+            foreach (var invalidPath in invalidPaths)
+                Assert.Throws<FileSystemException>(() => fileSystem.FullPath(invalidPath));
+        }
 
-        //    foreach (var invalidPath in invalidPaths)
-        //        Assert.Throws<FileSystemException>(() => fileSystem.FullPath(invalidPath));
-        //}
+        [Test]
+        public void TestLocalWithBracketsInPathCrossPlatform()
+        {
+            // Use a cross-platform base path with brackets
+            string basePath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) 
+                ? @"C:\Some (thing)\Path" 
+                : "/home/some (thing)/path";
+            
+            LocalFileSystem fileSystem = new LocalFileSystem(basePath);
+            
+            Assert.AreEqual(
+                Path.Combine(basePath, "_mypartial.liquid"), 
+                fileSystem.FullPath("mypartial"));
+            
+            Assert.AreEqual(
+                Path.Combine(basePath, "dir", "_mypartial.liquid"), 
+                fileSystem.FullPath("dir/mypartial"));
+        }
 
-        //[Test]
-        //[Category("windows")]
-        //public void TestLocalWithBracketsInPath()
-        //{
-        //    LocalFileSystem fileSystem = new LocalFileSystem(@"D:\Some (thing)\Path");
-        //    Assert.AreEqual(@"D:\Some (thing)\Path\_mypartial.liquid".Replace('\\', Path.DirectorySeparatorChar), fileSystem.FullPath("mypartial"));
-        //    Assert.AreEqual(@"D:\Some (thing)\Path\dir\_mypartial.liquid".Replace('\\', Path.DirectorySeparatorChar), fileSystem.FullPath("dir/mypartial"));
-        //}
+        [Test]
+        public void TestEmbeddedResource()
+        {
+            var assembly = typeof(FileSystemTests).GetTypeInfo().Assembly;
+            EmbeddedFileSystem fileSystem = new EmbeddedFileSystem(assembly, "DotLiquid.Tests.Embedded");
+            
+            foreach (var validPath in validPaths)
+            {
+                // For embedded resources, path separators become dots
+                string expectedResourceName = "DotLiquid.Tests.Embedded." + 
+                    validPath.Value.Replace(Path.DirectorySeparatorChar, '.').Replace('/', '.').Replace('\\', '.');
+                
+                Assert.AreEqual(
+                    expected: expectedResourceName,
+                    actual: fileSystem.FullPath(validPath.Key));
+            }
 
-        // TODO: Fix
-        //[Test]
-        //public void TestEmbeddedResource()
-        //{
-        //    var assembly = typeof(FileSystemTests).GetTypeInfo().Assembly;
-        //    EmbeddedFileSystem fileSystem = new EmbeddedFileSystem(assembly, "DotLiquid.Tests.Embedded");
-        //    foreach (var validPath in validPaths)
-        //        Assert.AreEqual(
-        //            expected: "DotLiquid.Tests.Embedded." + Liquid.DirectorySeparatorsRegex.Replace(validPath.Value, "."),
-        //            actual: fileSystem.FullPath(validPath.Key));
-
-        //    foreach (var invalidPath in invalidPaths)
-        //        Assert.Throws<FileSystemException>(() => fileSystem.FullPath(invalidPath));
-        //}
+            foreach (var invalidPath in invalidPaths)
+                Assert.Throws<FileSystemException>(() => fileSystem.FullPath(invalidPath));
+        }
     }
 }
